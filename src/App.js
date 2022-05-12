bc69d2e9a1daae811cf9cc61a54e06f933d61138
Johan Sora <johansp0305@gmail.com>
2022-05-11 22:04:37 -0500

    Add RegisterForm

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import RegisterForm from "./pages/RegisterForm.js";
 
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link>Hola</Link>
-      </nav>
       <Routes>
-        <Route />
-        <Route />
-        <Route />
+        <Route exact path="/register-form" element={<RegisterForm />} />
       </Routes>
     </BrowserRouter>
   );
