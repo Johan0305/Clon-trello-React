7ed4bed0e9bda47764d9e36fb896be578dc53c69
Daniela <dannytorres0211@gmail.com>
2022-05-16 21:34:10 -0500

    Create pages to error 404 and profile

--- src/components/dashboard/nav/NavLogo.js
+++ src/components/dashboard/nav/NavLogo.js
@@ -1,11 +1,12 @@
 import logoTrello from "../../../assets/logo/logo-white.svg";
+import { Link } from "react-router-dom";
 
 const NavLogo = () => {
     return(
         <figure className="nav-logo">
-            <a>
+            <Link to="/dashboard">
                 <img src={logoTrello} alt=""/>
-            </a>
+            </Link>
         </figure>
     );
 }
---
+++ src/pages/NotFound.js
@@ -0,0 +1,16 @@
+import notFoundImg from "../assets/illustrations/NotFound.jpg"
+import LandingHeaderBrand from "../components/landing/LandingHeaderBrand";
+
+const NotFound = () =>{
+    return(
+        <div className="notFound">
+        <img src={notFoundImg} alt="Error 404" className="notFound-img"></img>
+        <a className="notFound-ref" href='https://www.freepik.es/vectores/error-web'>Vector de error web creado por storyset - www.freepik.es</a>
+        <br/>
+        <p className="notFound-Paragraph"><strong>Not Found.</strong> Te invitamos a regresar a nuestra Home Page:</p>
+        <LandingHeaderBrand />
+        </div>
+    );
+};
+
+export default NotFound;
