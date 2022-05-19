7592a03b4dfb46ca76f2a99d9c4c08f4953d32c0
Daniela <dannytorres0211@gmail.com>
2022-05-19 12:37:53 -0500

    Create popover to Button notification and button profile

--- src/components/dashboard/nav/Nav.js
+++ src/components/dashboard/nav/Nav.js
@@ -1,16 +1,19 @@
 import NavLogo from "./NavLogo";
-import Avatar from "../Avatar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBell, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import ButtonRecent from "./ButtonRecent";
 import ButtonMark from "./ButtonMark";
 import ButtonCreate from "./ButtonCreate";
+import ButtonNotifications from "./ButtonNotifications";
+import ButtonProfile from "./ButtonProfile";
 import { useState } from "react";
 
 const Nav = () => {
   const [openModalRecent, setOpenModalRecent] = useState(false);
   const [openModalMark, setOpenModalMark] = useState(false);
   const [openModalCreate, setOpenModalCreate] = useState(false);
+  const [openModalNotificate, setOpenModalNotificate] = useState(false);
+  const [openModalProfile, setOpenModalProfile] = useState(false);
   return (
     <nav className="nav">
       <div className="nav-left">
@@ -26,14 +29,11 @@ const Nav = () => {
             <input type="text" placeholder="Buscar..."></input>
           </form>
         </div>
-        <div className="nav-notifications">
-          <a className="nav-notifications-icon">
-            <FontAwesomeIcon icon={faBell} />
-          </a>
-        </div>
-        <div className="nav-avatar">
-          <Avatar />
-        </div>
+        <ButtonNotifications
+          open={openModalNotificate}
+          setOpen={setOpenModalNotificate}
+        />
+        <ButtonProfile open={openModalProfile} setOpen={setOpenModalProfile} />
       </div>
     </nav>
   );
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,14 +1,5 @@
 import Nav from "../components/dashboard/nav/Nav";
 import BoardsMenu from "../components/dashboard/boards menu/BoardsMenu";
-import Popover from "../components/dashboard/Popover";
-import BoardThumbnail from "../components/dashboard/nav/BoardThumbnail";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
-import ActionButton from "../components/dashboard/ActionButton";
-import Notification from "../components/dashboard/nav/Notification";
-import Divider from "../components/dashboard/Divider";
-import Avatar from "../components/dashboard/Avatar";
-import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   return (
